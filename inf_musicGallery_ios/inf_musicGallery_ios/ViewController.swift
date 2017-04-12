000f1bc87a50967251d605b780c23ecb1c3d4a7c
kamil.buczel <kamil.buczel@naturaily.com>
2017-04-12 18:55:03 +0200

    Initial commit

---
+++ inf_musicGallery_ios/inf_musicGallery_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  inf_musicGallery_ios
+//
+//  Created by Kamil Buczel on 12.04.2017.
+//  Copyright © 2017 Kamajabu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
